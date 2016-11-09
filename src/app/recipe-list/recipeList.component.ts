92801aeed937040b8294d5100025d0c0519adf8f
Mateusz  Krupa <mateuszkrupa@MacBook-Pro-Mateusz.local>
2016-11-09 17:42:46 +0100

    recipe list items

--- src/app/recipe-list/recipeList.component.ts
+++ src/app/recipe-list/recipeList.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
     styleUrls: ['recipeList.component.sass']
 })
 export class RecipeListComponent { 
-  title = 'reciple List title';
+
+  item = {
+    name: "Schabowe w wciescie",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    image: "assets/img/ramen.jpg",
+    kcal: "300"
+  }
+
+  items = (function(item){
+    let items = [];
+    for(let i = 0; i < 30; i++){
+      let tmp = Object.create(item); 
+      tmp.name = item.name + i;
+      items.push(tmp)
+    }
+    return items;
+  })(this.item)
+
+  title = this.items;
+
 }
