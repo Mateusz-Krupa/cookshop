d0128569edaf673a09bddeea7a5f51b6faf113c9
Ewelina Dziewiela <ewelinadziewiela@MacBook-Pro-Ewelina.local>
2016-11-09 20:29:18 +0100

    recipe list and allergic icon

--- src/app/recipe-list/recipeList.component.ts
+++ src/app/recipe-list/recipeList.component.ts
@@ -5,19 +5,24 @@ import { Component } from '@angular/core';
     templateUrl: 'recipeList.component.html',
     styleUrls: ['recipeList.component.sass']
 })
-export class RecipeListComponent { 
+export class RecipeListComponent {
 
   item = {
     name: "Schabowe w wciescie",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
     image: "assets/img/ramen.jpg",
-    kcal: "300"
+    kcal: "300",
+    allergens: {
+      gluten: true,
+      lactose: true
+    }
+
   }
 
   items = (function(item){
     let items = [];
     for(let i = 0; i < 30; i++){
-      let tmp = Object.create(item); 
+      let tmp = Object.create(item);
       tmp.name = item.name + i;
       items.push(tmp)
     }
